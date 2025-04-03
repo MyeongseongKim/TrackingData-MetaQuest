b1ad50dee8ef32ab4f9733e5113a4854a053a655
MyeongseongKim <marqus.kim@gmail.com>
2025-04-03 18:54:09 +0900

    Refactor: static readonly variables

--- Assets/Scripts/TrackingDataManager.cs
+++ Assets/Scripts/TrackingDataManager.cs
@@ -13,17 +13,6 @@ using SerializableData;
 
 public class TrackingDataManager : MonoBehaviour
 {
-    public static readonly Dictionary<string, HandJointId[]> HAND_JOINTS = new Dictionary<string, HandJointId[]> {
-        { "Wrist", new[] {HandJointId.HandWristRoot} },  
-        { "Palm", new[] {HandJointId.HandPalm} }, 
-        { "Thumb", new[] {HandJointId.HandThumb1, HandJointId.HandThumb2, HandJointId.HandThumb3, HandJointId.HandThumbTip} }, 
-        { "Index", new[] {HandJointId.HandIndex1, HandJointId.HandIndex2, HandJointId.HandIndex3, HandJointId.HandIndexTip} }, 
-        { "Middle", new[] {HandJointId.HandMiddle1, HandJointId.HandMiddle2, HandJointId.HandMiddle3, HandJointId.HandMiddleTip} }, 
-        { "Ring", new[] {HandJointId.HandRing1, HandJointId.HandRing2, HandJointId.HandRing3, HandJointId.HandRingTip} }, 
-        { "Pinky", new[] {HandJointId.HandPinky1, HandJointId.HandPinky2, HandJointId.HandPinky3, HandJointId.HandPinkyTip} }  
-    };
-
-
     [SerializeField]
     private OVRCameraRig _cameraRig;
 
@@ -100,7 +89,7 @@ public class TrackingDataManager : MonoBehaviour
     {
         var handData = new SerializableHandData();
         
-        foreach (var pair in HAND_JOINTS) 
+        foreach (var pair in TrackingUtils.HAND_JOINTS) 
         {
             var name = pair.Key;
             var joints = pair.Value;
